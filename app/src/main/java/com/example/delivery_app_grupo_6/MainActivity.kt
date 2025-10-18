bc87c5969ab734ea5690040ac1a164cbed54a805
unknown <SSDD@PNAWSA05LC54524.duoc.acad>
2025-10-18 14:56:02 -0300

    Carga inicial

---
+++ app/src/main/java/com/example/delivery_app_grupo_6/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.delivery_app_grupo_6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.delivery_app_grupo_6.ui.theme.Delivery_app_Grupo_6Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Delivery_app_Grupo_6Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Delivery_app_Grupo_6Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/delivery_app_grupo_6/ui/screen/PantallaAjustes.kt
@@ -0,0 +1,3 @@
+package com.example.delivery_app_grupo_6.ui.screen
+
+import androidx.compose.runtime.Composable
