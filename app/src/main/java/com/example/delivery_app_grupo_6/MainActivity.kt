482d6c823e224427812553c8c52d9c1a204374a6
Guillermo <guillermosantander343@gmail.com>
2025-10-22 19:35:56 -0300

    DSNHFEI

--- app/src/main/java/com/example/delivery_app_grupo_6/MainActivity.kt
+++ app/src/main/java/com/example/delivery_app_grupo_6/MainActivity.kt
@@ -1,18 +1,13 @@
-package com.example.delivery_app_grupo_6
+package com.example.delivery_app_grupo_6.ui.theme
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import com.example.delivery_app_grupo_6.navigation.AppNavigation
-import com.example.delivery_app_grupo_6.ui.theme.Delivery_app_grupo_6Theme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Delivery_app_grupo_6Theme {
-                AppNavigation()
-            }
-        }
-    }
+@Composable
+fun DeliveryAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
 }
